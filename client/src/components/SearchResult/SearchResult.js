920054c1cd983d460ff257c255e84a8550766028
Robert Flanagan <robertflanagan95@gmail.com>
2019-09-12 14:44:47 -0400

    styling on search results, conditional rendering for poster images

--- client/src/components/SearchResult/SearchResult.js
+++ client/src/components/SearchResult/SearchResult.js
@@ -12,7 +12,7 @@ class SearchResult extends Component {
         return(
             <div className="search-result">
                 <div className="search-poster">
-                    <img src={"https://image.tmdb.org/t/p/w300" + this.props.content.poster_path} alt={"Poster for " + this.props.content.title} />
+                    {(this.props.content.poster_path) ? (<img src={"https://image.tmdb.org/t/p/w300" + this.props.content.poster_path} alt={"Poster for " + this.props.content.title} />) : (<img src={"https://via.placeholder.com/300"} alt={"No poster available for " + this.props.content.title} />)}
                 </div>
                 <div><span className="search-title">{this.props.content.title}</span><span className="search-release">{this.props.content.release_date}</span></div>
                 <div className="search-overview">{this.props.content.overview}</div>
